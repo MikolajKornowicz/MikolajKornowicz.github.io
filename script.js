2b66ce060d5c5781835907a8b4034ecf1a35e4ef
Mikolaj Kornowicz <mikolaj.kornowicz@gmail.com>
2022-04-02 18:28:20 +0200

    deleteFix

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ $(document).ready(function() {
     var requestUrl = apiRoot + taskId;
 
     $.ajax({
-      url: requestUrl;,
+      url: requestUrl,
       method: 'DELETE',
       success: function() {
         parentEl.slideUp(400, function() { parentEl.remove(); });
